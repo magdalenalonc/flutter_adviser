5c27b5ce139eeafca9ff1f54db77fbcc7f807dd8
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-01-09 18:49:56 +0100

    Add equatable to BLoC - to make testing easier later

--- lib/2_application/pages/advice/bloc/adviser_state.dart
+++ lib/2_application/pages/advice/bloc/adviser_state.dart
@@ -1,7 +1,10 @@
 part of 'adviser_bloc.dart';
 
 @immutable
-sealed class AdviserState {}
+sealed class AdviserState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 final class AdviserInitial extends AdviserState {}
 
@@ -10,9 +13,15 @@ final class AdviserStateLoading extends AdviserState {}
 final class AdviserStateLoaded extends AdviserState {
   AdviserStateLoaded({required this.advice});
   final String advice;
+
+  @override
+  List<Object?> get props => [advice];
 }
 
 final class AdviserStateError extends AdviserState {
   AdviserStateError({required this.message});
   final String message;
+
+  @override
+  List<Object?> get props => [message];
 }
