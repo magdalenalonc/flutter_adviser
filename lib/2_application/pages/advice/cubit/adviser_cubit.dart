7290cdd89616d32978731d14f1b31d7112a0f649
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-01-10 23:46:38 +0100

    Add failures & dartz

--- lib/2_application/pages/advice/cubit/adviser_cubit.dart
+++ lib/2_application/pages/advice/cubit/adviser_cubit.dart
@@ -1,9 +1,13 @@
-import 'package:flutter_adviser/1_domain/entities/advice_entity.dart';
+import 'package:flutter_adviser/1_domain/failures/failures.dart';
 import 'package:flutter_adviser/1_domain/usecases/advice_usecases.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'adviser_state.dart';
+
+const generalFailureMessage = 'Ups, something gone wrong. Please try again!';
+const serverFailureMessage = 'Ups, API error. Please try egain!';
+const cacheFailureMessage = 'Ups, cache failed. Please try egain!';
 
 class AdviserCubit extends Cubit<AdviserCubitState> {
   AdviserCubit() : super(AdviserInitial());
@@ -13,9 +17,24 @@ class AdviserCubit extends Cubit<AdviserCubitState> {
   // bloc - has events; cubit - has functions
   void adviceRequestedEvent() async {
     emit(AdviserStateLoading());
-    final AdviceEntity advice = await adviceUseCases.getAdvice();
 
-    emit(AdviserStateLoaded(advice: advice.advice));
-    // emit(const AdviserStateError(message: 'error message'));
+    final failureOrAdvice = await adviceUseCases.getAdvice();
+
+    failureOrAdvice.fold(
+      (failure) =>
+          emit(AdviserStateError(message: _mapFailureToMessage(failure))),
+      (advice) => emit(AdviserStateLoaded(advice: advice.advice)),
+    );
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return serverFailureMessage;
+      case CacheFailure:
+        return cacheFailureMessage;
+      default:
+        return generalFailureMessage;
+    }
   }
 }
